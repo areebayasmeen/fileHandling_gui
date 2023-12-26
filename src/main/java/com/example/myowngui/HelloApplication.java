83bd3b29a64f7daeca035a09acc2d8fe17221655
areebayasmeen <areebayasmeen11@gmail.com>
2023-12-26 19:23:11 +0500

    fileHandlingWithGUI

--- src/main/java/com/example/myowngui/HelloApplication.java
+++ src/main/java/com/example/myowngui/HelloApplication.java
@@ -23,46 +23,37 @@ public class HelloApplication extends Application {
     Members members=new Members();
     TextField nameField=new TextField() ;
     TextField ageField = new TextField();
-     String line;
-     TextArea textArea = new TextArea();
+    // TextArea textArea = new TextArea();
     RadioButton male=new RadioButton("male");
-
     RadioButton female=new RadioButton("female");
     ToggleGroup toggleGroup=new ToggleGroup();
     String selectedOption=null;
     String membershipType;
     ComboBox<String> comboBox=new ComboBox<>();
     DatePicker datePicker=new DatePicker();
-  LocalDate date;
+    LocalDate date;
     TableView<Members> tableView = new TableView<>();
-    ObservableList<Members> membersList = FXCollections.observableArrayList();
+    ObservableList<Members> membersList ;
 
 
+//start method
     @Override
     public void start(Stage stage) throws IOException {
 
         GridPane grid = new GridPane();
-        // grid.setGridLinesVisible(true);
         grid.setHgap(10);
         grid.setVgap(10);
         Scene scene =new Scene(grid,600,500);
 
-        tableView.setEditable(true);
-
-
-
-
+        membersList = FXCollections.observableArrayList(new Members());
         Button backButtonforDetails=new Button("back");
         Button addMembers =new Button("Add Members");//button to take user to the form where he fills out his credentials
         Button button=new Button("add");// button to add user details to file
         Button show_details=new Button("show details");//button to take user to details form
         Button select_membership_type=new Button("select");
-        Button SelectDate=new Button("date");
         datePicker.setValue(LocalDate.now());
         comboBox.setItems(FXCollections.observableArrayList(
                 "Standard", "Premium", "VIP")); // Add items to the ComboBox
-
-
         male.setToggleGroup(toggleGroup); // Add RadioButton to the ToggleGroup
         male.setSelected(true);
         female.setToggleGroup(toggleGroup);
@@ -83,7 +74,7 @@ public class HelloApplication extends Application {
             }
         });
 
-
+//button to add members on main scene
     addMembers.setOnAction( e ->{
 
     GridPane gridPane=new GridPane() ;
@@ -93,12 +84,12 @@ public class HelloApplication extends Application {
     gridPane.add(nameField, 3, 2);
     gridPane.add(ageField, 3, 3);
     gridPane.add(backbutton,200,100);
-   gridPane.add(male,3,5);
-   gridPane.add(female,3,6);
-gridPane.add(select_membership_type,3,7);
-gridPane.add(comboBox,3,8);
-gridPane.add(datePicker,3,9);
-datePicker.setOnAction(actionEvent -> {
+    gridPane.add(male,3,5);
+    gridPane.add(female,3,6);
+    gridPane.add(select_membership_type,3,7);
+    gridPane.add(comboBox,3,8);
+    gridPane.add(datePicker,3,9);
+    datePicker.setOnAction(actionEvent -> {
     datepicker();
 });
 select_membership_type.setOnAction(actionEvent -> {
@@ -106,31 +97,29 @@ select_membership_type.setOnAction(actionEvent -> {
 
 });
 
-   button.setOnAction( e1 -> {
+   button.setOnAction( e1 -> {//button to save member details to file
 
             addMembers();
 
         });
-backbutton.setOnAction(actionEvent -> {
+backbutton.setOnAction(actionEvent -> {//button to go back to main scene
     try {
         start(stage);
     } catch (IOException ex) {
         throw new RuntimeException(ex);
     }
 });
-    stage.setScene(scene1);
+   stage.setScene(scene1);
    stage.show();
 
 });
-
-        show_details.setOnAction( e1 -> {
+//button to show member details on main scene
+        show_details.setOnAction( e1 -> { //this is where the problem is present
 
             GridPane gridPane1=new GridPane() ;
             Scene scene2=new Scene(gridPane1,600,500);
             gridPane1.add(tableView, 1, 4);
             gridPane1.add(backButtonforDetails,100,100);
-
-           // TableView<Members> tableView = new TableView<>();
 
             TableColumn<Members, String> nameColumn = new TableColumn<>("Name");
             nameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
@@ -148,12 +137,12 @@ backbutton.setOnAction(actionEvent -> {
             dateColumn.setCellValueFactory(new PropertyValueFactory<>("date1"));
 
             tableView.getColumns().addAll(nameColumn, ageColumn, genderColumn, membershipColumn, dateColumn);
-            tableView.setItems(membersList);
-
+            tableView.setItems(readMembers());
+            tableView.refresh();
 //            gridPane1.add(textArea,3,5);
 //            textArea.setEditable(false);
-             showMembers();
-            backButtonforDetails.setOnAction(actionEvent -> {
+             //showMembers();
+            backButtonforDetails.setOnAction(actionEvent -> { //backbutton from details scene
                 try {
                     start(stage);
                 } catch (IOException ex) {
@@ -169,9 +158,8 @@ backbutton.setOnAction(actionEvent -> {
         nameField.setPromptText("name");
         ageField.setPromptText("age");
 
-
-       grid.add(addMembers,1,2);
-        grid.add(show_details,1,3);
+        grid.add(addMembers,1,2);
+        grid.add(show_details,1,3);//adding buttons to main scene
         stage.setScene(scene);
         stage.show();
     }
@@ -182,15 +170,11 @@ backbutton.setOnAction(actionEvent -> {
   void datepicker(){
         date=datePicker.getValue();
   }
-
+//method to write member to file
     void addMembers(){
 
-
-        HelloApplication helloApplication=new HelloApplication();
         String name = nameField.getText();
         String age = ageField.getText();
-
-
 
                 try {
             FileWriter fileWriter = new FileWriter("memebers.txt", true);
@@ -206,28 +190,29 @@ backbutton.setOnAction(actionEvent -> {
                         selectedOption = female.getText();
                     }
                     bufferedWriter.write(" ");
-             bufferedWriter.write(selectedOption  );
+                    bufferedWriter.write(selectedOption  );
 
                     if(membershipType!=null){
                         bufferedWriter.write(" ");
-             bufferedWriter.write(membershipType);
+                        bufferedWriter.write(membershipType);
                       }
-                    if(date!=null){  bufferedWriter.write(" "); bufferedWriter.write(String.valueOf(date  ));}
+                    if(date!=null){  bufferedWriter.write(" ");
+                                     bufferedWriter.write(String.valueOf(date  ));}
                     bufferedWriter.newLine();
-            bufferedWriter.close();
+                    bufferedWriter.close();
 
         }
-
 
         catch (IOException e) {
             throw new RuntimeException(e);
         }
 
-
     }
 
-    public void showMembers() {
-      //  StringBuilder content = new StringBuilder();
+// method to read members from file and return a list of members
+    //this could contain error too
+    public ObservableList<Members> readMembers() {
+        //  StringBuilder content = new StringBuilder();
 
         try {
             FileReader   fileReader = new FileReader("memebers.txt");
@@ -239,7 +224,7 @@ backbutton.setOnAction(actionEvent -> {
 
             while ((line = bufferedReader.readLine()) != null) {
                 String[] parts = line.split("\\s+");
-
+int i=0;
                 if (parts.length == 5) {
                     String name = parts[0];
                     int age = Integer.parseInt(parts[1]);
@@ -248,8 +233,9 @@ backbutton.setOnAction(actionEvent -> {
                     String date1=parts[4];
 
 
-                    Members person = new Members(name, age, gender,membership,date1);
-                    membersList.add(person);
+                    membersList.add(new Members(name, age, gender, membership, date1));
+
+
                 }
             }
 //            textArea.setText(content.toString());
@@ -258,8 +244,7 @@ backbutton.setOnAction(actionEvent -> {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-
-    }
+        return membersList;  }
 
 
     public static void main(String[] args) {
